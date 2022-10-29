362c343a1f7c55555c7c1abd48bced06905498d7
Manuel Espinoza <manuelespv@gmail.com>
2022-10-29 17:25:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen bg-slate-700 text-slate-100 text-3xl flex justify-center items-center">
+      UNDER CONSTRUCTION 🔨🔧
+      <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+    </div>
+  )
+}
+
+export default App
